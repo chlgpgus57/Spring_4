6dd4f215fb0f28fc8fd4b23bee02129b764628ec
chlgpgus57 <55524992+chlgpgus57@users.noreply.github.com>
2019-11-28 09:39:03 +0900

    add_ignore

--- src/main/java/com/iu/s4/controller/JsonController.java
+++ src/main/java/com/iu/s4/controller/JsonController.java
@@ -4,9 +4,7 @@ import java.util.List;
 
 import javax.inject.Inject;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
